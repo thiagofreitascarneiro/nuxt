0cc5041e207225b570f3994f0aba127f0c389087
Thiago <thiagofreitascarneiro@hotmail.com>
2022-11-22 18:16:07 -0300

    fazendo busca de dados com asyncData

--- pages/produtos/index.vue
+++ pages/produtos/index.vue
@@ -3,8 +3,17 @@
     <div>
         <div class="container mx-auto">
             <h1 class="text-lg font-bold">Produtos</h1>
+            <NuxtLink to="/produtos/produto-a">Produto A</NuxtLink>
+            <NuxtLink to="/produtos/produto-b">Produto B</NuxtLink>
+
+            <br/>
+            <br/>
+
+            <div v-for="product in products" :key="product.id" class="border-b border-400 py-3">
+                {{ product.title }}
+            </div>
+
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus tempore non accusamus, 
-                ratione alias aliquam dicta assumenda aspernatur quasi fugiat corrupti eos dolorem sequi autem id! Doloribus odio labore nam!
             </p>
         </div>
     </div>
@@ -12,7 +21,25 @@
 </template>
 
 <script>
+
+
 export default {
+    name: '',
+
+    data() {
+        return {
+
+        };
+    },
+
+    async asyncData({ $axios }) {
+         const products = await $axios.$get('https://jsonplaceholder.typicode.com/posts?_limit=3')
+         return {
+           products,
+         }
+       },
+
+    methods: {},
 
 }
 </script>
