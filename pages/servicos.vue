84ae10e2f5993b53d79dd822488b57dc484d6f4d
Thiago <thiagofreitascarneiro@hotmail.com>
2022-11-22 14:44:56 -0300

    criando projeto de financas com nuxt

---
+++ pages/servicos.vue
@@ -0,0 +1,32 @@
+<template>
+     <div>
+    <div class="bg-gray-100">
+        <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
+            <NuxtLink to="/a-empresa">A empresa</NuxtLink>
+            <NuxtLink to="/servicos">Serviços</NuxtLink>
+        </div>
+
+    </div>
+  
+
+    <div>
+        <div class="container mx-auto">
+            <h1 class="text-lg font-bold">Serviços</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus tempore non accusamus, 
+                ratione alias aliquam dicta assumenda aspernatur quasi fugiat corrupti eos dolorem sequi autem id! Doloribus odio labore nam!
+            </p>
+        </div>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
